148273afb7d46525ee99d64b7933891263fd4b1a
Max Zavaleta <mzavaletav@gmail.com>
2023-10-23 22:24:50 -0500

    feat: houses and departament in partial

--- controller/appController.js
+++ controller/appController.js
@@ -5,15 +5,45 @@ const  home = async (req,res)=>{
 
     //const categories = await Category.findAll()
     
-    const [categories, prices ]= await Promise.all([
+    const [categories, prices, ids1, ids2 ]= await Promise.all([
         Category.findAll({raw : true}),
-        Price.findAll({raw : true})
+        Price.findAll({raw : true}),
+        Property.findAll({
+            limit: 3,
+            where: {
+                categoryId: 1
+            },
+            include:[
+                {
+                    model: Price,
+                    as: 'price'
+                }
+            ],
+            order:[ ['createdAt','DESC']]
+        }),
+        Property.findAll({
+            limit: 3,
+            where: {
+                categoryId: 2
+            },
+            include:[
+                {
+                    model: Price,
+                    as: 'price'
+                }
+            ],
+            order:[ ['createdAt','DESC']]
+        })
     ])
+    console.log(ids1);
+    console.log(ids2);
     res.render("home",{
         page:"Inicio",
         bar:true,
         categories,
-        prices
+        prices,
+        ids1,
+        ids2
     })
 }
 
